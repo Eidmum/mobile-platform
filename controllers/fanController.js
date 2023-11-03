fa5288f88ebede5b6ebec29dc56109f8b867d240
eidmum <mdzuneadabedineidmum@gmail.com>
2023-11-04 02:41:31 +0600

    error checked

--- controllers/fanController.js
+++ controllers/fanController.js
@@ -7,6 +7,6 @@ exports.getState = async (req, res) => {
 exports.updateState = async (req, res) => {
   console.log(req.body);
   const a = await Fan.updateOne({ name: req.body.name }, { $set: req.body });
-  console.log(a);
+
   res.status(200).json({ message: "successfull" });
 };
--- controllers/ledController.js
+++ controllers/ledController.js
@@ -1,7 +1,7 @@
 const { set } = require("../app");
 const Led = require("../models/ledModel");
 exports.getState = async (req, res) => {
-  const state = await Led.findOne({ name: "led" });
+  const state = await Led.findOne({ name: "light" });
   res.status(200).json({ state });
 };
 exports.updateState = async (req, res) => {
